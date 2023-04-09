6da2c75980de67484cd5949fa32b4e566ec75a6f
SeifEldidi <106155115+SeifEldidi@users.noreply.github.com>
2023-04-09 13:55:20 +0200

    Update RTCDS1307.c

--- RTCDS1307/RTCDS1307.c
+++ RTCDS1307/RTCDS1307.c
@@ -6,13 +6,14 @@
  */
 #include "RTCDS1307.h"
 
+
 Bool RTCDS1307_SET_TIME(uint8 Mode,uint8 Sec,uint8 Mins,uint8 Hrs)
 {
 	Bool E_stat=E_OK;
-	uint8 Time_Arr[4]={0};
+	uint8 Time_Arr[3];
 	if((Sec>59)||(Mins>59))
 		E_stat=E_NOK;
-	if((Mode==MODE_24H&&Hrs>23)||(Hrs>12))
+	if((Mode==MODE_24H&&Hrs>23)||(Mode!=MODE_24H&&Hrs>12))
 		E_stat=E_NOK;
 	if(E_stat==E_OK)
 	{
@@ -58,7 +59,7 @@ Bool RTCDS1307_GET_TIME(uint8 *Time_Buff)
 {
 	Bool E_stat=E_OK;
 	uint8 Temp;
-	I2C_READ_NBYTES(RTCDS1307_ADD,RTCDS1307_SEC_REG,Time_Buff,3);
+	E_stat=I2C_READ_NBYTES(RTCDS1307_ADD,RTCDS1307_SEC_REG,Time_Buff,3);
 	Time_Buff[0]=(Temp>>4)*10+(Time_Buff[0]&0x0F);
 	Time_Buff[1]=((Time_Buff[1])>>4)*10+(Time_Buff[1]&0xF);
 	Temp=Time_Buff[2]&0xE0;
@@ -67,11 +68,113 @@ Bool RTCDS1307_GET_TIME(uint8 *Time_Buff)
 	return E_stat;
 }
 
-Bool RTCDS1307_GET_DATA(uint8 *Date_Buff)
+Bool RTCDS1307_GET_DATE(uint8 *Date_Buff)
 {
 	Bool E_stat=E_OK;
-
+	E_stat=I2C_READ_NBYTES(RTCDS1307_ADD,RTCDS1307_DAY_REG,Date_Buff,4);
+	*Date_Buff=*Date_Buff&0x07;
+	*(Date_Buff+1)=(*(Date_Buff+1)>>4)*10+*(Date_Buff+1);
+	*(Date_Buff+2)=(*(Date_Buff+2)>>4)*10+*(Date_Buff+2);
+	*(Date_Buff+3)=(*(Date_Buff+3)>>4)*10+*(Date_Buff+3);
 	return E_stat;
 }
 
+Bool RTCDS1307_TIME_STR(uint8 *Time_Buff)
+{
+	Bool E_stat = E_OK;
+	Bool Hrs_flag=0;
+	uint8 Temp_Time_Buff[3];
+	E_stat = I2C_READ_NBYTES(RTCDS1307_ADD, RTCDS1307_SEC_REG,Temp_Time_Buff, 3);
+	if(BIT_IS_SET(Temp_Time_Buff[2],TW_TFR_MODE_POS))
+	{
+		Time_Buff[0]=ZERO+((Temp_Time_Buff[2]&0x10)>>4);
+		Time_Buff[1]=ZERO+(Temp_Time_Buff[2]&0x0F);
+		if(BIT_IS_SET(Temp_Time_Buff[2],AM_PM_MODE_POS))
+			Hrs_flag=1;//PM
+		else
+			Hrs_flag=2;//AM
+	}
+	else
+	{
+		Time_Buff[0]=ZERO+((Temp_Time_Buff[2]&0x30)>>4);
+		Time_Buff[1]=ZERO+(Temp_Time_Buff[2]&0x0F);
+	}
+	Time_Buff[2] = ':';
+	Time_Buff[3] = ZERO + (Temp_Time_Buff[1] >> 4);
+	Time_Buff[4] = ZERO + (Temp_Time_Buff[1] & 0x0F);
+	Time_Buff[5] = ':';
+	Time_Buff[6] = ZERO + (Temp_Time_Buff[0] >> 4);
+	Time_Buff[7] = ZERO + (Temp_Time_Buff[0] & 0x0F);
+	if(Hrs_flag==1)
+	{
+		Time_Buff[8] = ' ';
+		Time_Buff[9] = 'P';
+		Time_Buff[10] = 'M';
+		Time_Buff[11] = NULLChar;
+	}
+	else if(Hrs_flag==2)
+	{
+		Time_Buff[8] = ' ';
+		Time_Buff[9] = 'A';
+		Time_Buff[10] = 'M';
+		Time_Buff[11] = NULLChar;
+	}
+	else{
+		Time_Buff[8] = NULLChar;
+	}
+	return E_stat;
+}
+
+static void Strcpy(uint8 *Dest,int8 *Src)
+{
+	uint8 Cnt=0;
+	while(*(Src+Cnt)!='\0')
+	{
+		*(Dest+Cnt)=*(Src+Cnt);
+		Cnt++;
+	}
+}
+
+Bool RTCDS1307_DATE_STR(uint8 *Date_Buff)
+{
+	Bool E_stat = E_OK;
+	uint8 Temp_Date_Buff[4];
+	E_stat = I2C_READ_NBYTES(RTCDS1307_ADD, RTCDS1307_DAY_REG, Temp_Date_Buff, 4);
+	Date_Buff[0]=ZERO+(Temp_Date_Buff[1]>>4);
+	Date_Buff[1]=ZERO+(Temp_Date_Buff[1]&0x0F);
+	Date_Buff[2]='/';
+	Date_Buff[3] = ZERO + (Temp_Date_Buff[2] >> 4);
+	Date_Buff[4] = ZERO + (Temp_Date_Buff[2] & 0x0F);
+	Date_Buff[5] = '/';
+	Date_Buff[6] = '2';
+	Date_Buff[7] = '0';
+	Date_Buff[8] = ZERO + (Temp_Date_Buff[3] >> 4);
+	Date_Buff[9] = ZERO + (Temp_Date_Buff[3] & 0x0F);
+	Date_Buff[10] = '/';
+	switch(Temp_Date_Buff[0])
+	{
+		case SUNDAY:
+			Strcpy(Date_Buff+10," Sun");
+			break;
+		case MONDAY:
+			Strcpy(Date_Buff+10," Mon");
+			break;
+		case TUESDAY:
+			Strcpy(Date_Buff+10," Tue");
+			break;
+		case WEDNESDAY:
+			Strcpy(Date_Buff+10," Wed");
+			break;
+		case THURSDAY:
+			Strcpy(Date_Buff+10," Thur");
+			break;
+		case FRIDAY:
+			Strcpy(Date_Buff+10," Fri");
+			break;
+		case SATURDAY:
+			Strcpy(Date_Buff+10," Sat");
+			break;
+	}
+	return E_stat;
+}
 
